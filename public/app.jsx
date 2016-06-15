c551f0da7766783b115f36687b457ddbfd9f6da7
Paul Campbell <paulcampbell@pcampbellHLlaptop.local>
2016-06-15 17:31:38 -0500

    added msg props

--- public/app.jsx
+++ public/app.jsx
@@ -1,23 +1,28 @@
 var Greeter = React.createClass({
 	getDefaultProps: function (){
 		return {
-			name: 'React'
+			name: 'React',
+			msg: 'from the component'
 		};
 	},
 	render: function () {
 		var name = this.props.name;	
+		var msg = this.props.msg;
 
-		
+
 		return (
 			<div>
 				<h1>Hello {name} Yall via JSX</h1>
-				<p>form the component</p>
+				<p>{msg + '!!'}</p>
 			</div>
 		);
 	}
 });
 
+var firstName = "PJC";
+var message = "frmoTheProp"
+
 ReactDOM.render(
-	<Greeter name="Paul"/>,
+	<Greeter name={firstName} msg={message}/>,
 	document.getElementById('app')
 );
