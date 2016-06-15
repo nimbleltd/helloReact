71162b84ab5734d54c5aa5bbf72cd9397d728f4b
Paul Campbell <paulcampbell@pcampbellHLlaptop.local>
2016-06-15 16:17:46 -0500

    using first compoment

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,15 @@
+var Greeter = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<h1>Hello React Yall via JSX</h1>
+				<p>form the component</p>
+			</div>
+		);
+	}
+});
+
 ReactDOM.render(
-	<h1>Hello React Yall via JSX</h1>,
+	<Greeter/>,
 	document.getElementById('app')
 );
